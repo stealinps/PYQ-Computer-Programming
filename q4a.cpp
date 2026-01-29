8789037f314e1712b30fb2cca5cf4706ff4c7c69
Stealinp <stealinp@gmail.com>
2026-01-29 23:51:29 +0800

    Refactor main function to use studentData struct

--- q4a.cpp
+++ q4a.cpp
@@ -2,18 +2,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+#include <string>
 using namespace std;
-int main(){
-  string name,matrix,cgpa;
-  ifstream inputFile("student.txt", ios::in);
+
+struct studentData {
+    string name;
+    string matrix;
+    string cgpa;
+};
+
+int main() {
+    studentData data;
+    ifstream inputFile("student.txt", ios::in);
     if (!inputFile) {
         cerr << "File could not be opened \n";
         exit(1);
     }
-  cout <<"Name:    Matrix ID:    CGPA:\n";
-  while (inputFile >> name >> matrix >> cgpa){
-    cout <<name<<" "<<matrix<<" "<<cgpa<<"\n";
-  }
-  inputFile.close();
-  
+    cout << "Name:    Matrix ID:    CGPA:\n";
+    while (inputFile >> data.name >> data.matrix >> data.cgpa) {
+        cout << data.name << " " << data.matrix << " " << data.cgpa << "\n";
+    }
+    inputFile.close();
+
 }
