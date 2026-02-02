7650bc75dad8fbd3148db10af1d8734ceb90ca46
Stealinp <stealinp@gmail.com>
2026-02-03 02:05:31 +0800

    Add age eligibility check for voting

---
+++ 1a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"Enter age ";
+  double age;
+  cin >>age;
+  if(age>=18){
+    cout<<"You age eligible to vote";
+  }
+  else{
+    cout <<"You are not eligible to vote";
+  }
+}
